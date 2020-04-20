a7d646d49580b2bd9f439c0fcbc7e3307c29b164
Moez-Hamza <hamza.moez@gmail.com>
2020-04-20 14:02:17 +0100

    first commit

---
+++ Quizz.js
@@ -0,0 +1,29 @@
+var questions =[{
+	question:"Which artist painted this ?",
+	answers:{
+		a:"Picasso",
+		b:"Van Gogh",
+		c:"Monet",	},
+	correctAnswer:"a"
+},
+	{
+	question:"In which movement belongs the Nymphéas of Claude Monet",
+	answers:{
+		a:"Expressionism",
+		b:"Imrepssionism",
+		c:"Contemporary"
+	},
+	correctAnswer:"b"	
+},
+	{
+	question:"What's the name of this painting ?",
+	answers:{
+		a:"Starry Night",
+		b:"Long Night",
+		c:"Cosmic Night"
+	},
+	correctAnswer:"a",
+},
+
+
+];
